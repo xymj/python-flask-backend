aaa0e5b4cb590e50bdb2f947354094ce7f128639
Ljt <liujiangtao.ljt@alibaba-inc.com>
2025-04-15 20:56:44 +0800

    add env load and log

--- src/flask_backend/app.py
+++ src/flask_backend/app.py
@@ -1,8 +1,31 @@
+import logging
+
 from flask import Flask, jsonify, request
 from flask_backend.controllers.web import web_bp  # 导入控制器蓝图
 from flask_migrate import Migrate
 from flask_backend.database import db
 from flask_backend.models import *
+
+# 配置日志记录器
+logging.basicConfig(
+    level=logging.INFO,  # 设置日志级别为 INFO
+    format='%(asctime)s - %(levelname)s - %(message)s',  # 设置日志格式
+)
+
+from dotenv import load_dotenv
+import os
+
+# 加载 .env 文件中的环境变量
+load_dotenv()
+
+# 现在可以使用 os.getenv 获取环境变量的值
+flask_app = os.getenv("FLASK_APP")
+flask_env = os.getenv("FLASK_ENV")
+secret_key = os.getenv("SECRET_KEY")
+database_url = os.getenv("DATABASE_URL")
+print(f'flask_app:{flask_app}, database_url: {database_url}')
+logging.info(f'flask_app:{flask_app}, database_url: {database_url}')
+
 app = Flask(__name__)
 # 注册蓝图到主应用
 app.register_blueprint(web_bp)
